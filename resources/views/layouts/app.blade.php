4a7edf906b05be4adb7279fc2fa24266cd3e2a3a
= <=>
2022-02-21 22:00:28 -0600

    La página principal está lista

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -33,7 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <img src="img/lOGO.png">
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -41,11 +41,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Acceder') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                                 </li>
                             @endif
                         @else
@@ -75,6 +75,86 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+         <!-- Footer -->
+    <footer id="footer" class="pb-4 pt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    <div class="row">
+                        <div class="col-12 col-lg-12 col-med-12">
+                            <h5>Sobre nosotros</h5>
+                        </div>
+                        <div class="col-12 col-lg-12 col-med-12">
+                            <p>Misión</p>
+                        </div>
+                        <div class="col-12 col-lg-12 col-med-12">
+                            <p>Visión</p>
+                        </div>
+                        <div class="col-12 col-lg-12 col-med-12">
+                          <p>Blog</p>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-3">
+                  <div class="row">
+                      <div class="col-12 col-lg-12 col-med-12">
+                          <h5>Ayuda y apoyo</h5>
+                      </div>
+                      <div class="col-12 col-lg-12 col-med-12">
+                          <p>Información de envío</p>
+                      </div>
+                      <div class="col-12 col-lg-12 col-med-12">
+                          <p>Devolución</p>
+                      </div>
+                      <div class="col-12 col-lg-12 col-med-12">
+                        <p>Rastreo</p>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-12 col-lg-3">
+                 <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <h5>Servicio al cliente</h5>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <p>Contáctanos</p>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <p>Rastreo</p>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                      <p>Puntos</p>
+                  </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+              <div class="row">
+                 <div class="col-12 col-lg-12 col-med-12">
+                     <h5>Encuentranos en</h5>
+                 </div>
+                 <div class="col-12 col-lg-12 col-med-12">
+                     <img src="img/hola.PNG">
+                 </div>
+             </div>
+        </div>
+        </div>
+    </footer>
+  
+    <div id="footerDivider" class="pt-3">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-6 col-12">
+                  <p>BOX BOOK JOSE, Copyright 2020. Todos los derechos reservados</p>
+              </div>
+              <div class="col-lg-6 col-12 text-rigth">
+                  <a href="">Noticias de privacidad</a>
+              </div>
+          </div>
+      </div>
+  </div>
+      <!-- EndFooter -->
+
     </div>
 </body>
 </html>
